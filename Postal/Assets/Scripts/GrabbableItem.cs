c8abc6281a3f36abb2e5662a3ab3455d06b77b66
JoshZimmerling <joshua.zimmerling@gmail.com>
2026-04-19 15:07:56 -0400

    Comments

--- Postal/Assets/Scripts/GrabbableItem.cs
+++ Postal/Assets/Scripts/GrabbableItem.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class GrabbableItem : MonoBehaviour
 {
-    private GameObject parentObj;
-    private bool isGrabbed;
-    private Vector2 offset;
     private Rigidbody2D rb;
 
+    //grabbing stuff
+    private bool isGrabbed = false;
+    private GameObject parentObj;
+    private Vector2 offset;
+
     //throwing stuff
+    private bool thrown = false;
     private float throwStartingSpeed = 8.5f;
     private float throwSlowingMultiplier = 0.965f;
     private float throwMinimumSpeed = 0.25f;
-    private bool thrown = false;
     private Vector2 throwDirection;
 
     //spell stuff
@@ -22,25 +24,20 @@ public class GrabbableItem : MonoBehaviour
     private float recallSlowingMultiplier = 0.925f;
     private float recallMinimumSpeed = 0.35f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         parentObj = null;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate()
     {
+        //If grabbed, place the package on the right side of the player
         if (isGrabbed) 
         { 
             UpdateOffset();
         }
+        //If the package is thrown (in flight), slowly reduce its speed until it hits throwMinimumSpeed, then conside it no longer thrown
         if (thrown)
         {
             rb.linearVelocity *= throwSlowingMultiplier;
@@ -50,6 +47,7 @@ public class GrabbableItem : MonoBehaviour
                 thrown = false;
             }
         }
+        //If the recall spell has been cast on the object, move it towards the player until it gets close enough
         if (recalled)
         {
             if (!postRecall)
@@ -77,6 +75,7 @@ public class GrabbableItem : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Deals with thrown object bouncing off of the wall
         if (collision.gameObject.tag == "Wall")
         {
             float currentSpeed = rb.linearVelocity.magnitude;
@@ -95,6 +94,7 @@ public class GrabbableItem : MonoBehaviour
         }
     }
 
+    //When an object gets grabbed, set the needed fields and child it to the player
     public void Grabbed(GameObject parent)
     {
         gameObject.tag = "Grabbed";
@@ -108,14 +108,7 @@ public class GrabbableItem : MonoBehaviour
         postRecall = false;
     }
 
-    public void Recalled(GameObject parent)
-    {
-        parentObj = parent;
-        rb.linearVelocity = Vector2.zero;
-        recalled = true;
-        thrown = false;
-    }
-
+    //Place the object on the side of the player according to the direction they are facing
     public void UpdateOffset()
     {
 
@@ -139,6 +132,7 @@ public class GrabbableItem : MonoBehaviour
         this.gameObject.transform.localPosition = offset;
     }
 
+    //When no longer carrying the package, update its fields to show it has been dropped
     public void Dropped()
     {
         gameObject.tag = "Grabbable";
@@ -148,6 +142,7 @@ public class GrabbableItem : MonoBehaviour
         isGrabbed = false;
     }
 
+    //Set an initial velocity for a thrown object and set the field "thrown" to true so that it can be tracked in Update
     public void Thrown(Vector2 target)
     {
         thrown = true;
@@ -157,5 +152,14 @@ public class GrabbableItem : MonoBehaviour
         rb.linearVelocity = throwDirection * throwStartingSpeed;
 
         Dropped();
+    }
+
+    //Set the object as recalled and give it the parent object to track where it needs to go
+    public void Recalled(GameObject parent)
+    {
+        parentObj = parent;
+        rb.linearVelocity = Vector2.zero;
+        recalled = true;
+        thrown = false;
     }
 }
